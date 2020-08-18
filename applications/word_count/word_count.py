6d9d1156c3a42692cb2f549d07629de4355aef7d
Tyler Alsop <tylerjalsop@gmail.com>
2020-08-17 18:24:25 -0600

    Word count function now outputs a list of strings, one for each word, all lowercase, no puntuations.

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,6 +1,42 @@
+import re
+
 def word_count(s):
     # Your code here
     words = {}
+    punctuation_to_ignore = '''":;,.-+=/\|[]{}()*^&'''
+
+
+################### With Python ###################
+    # for character in s:
+    #     if character in punctuation_to_ignore:
+    #         s = s.replace(character, "")
+
+    lowercase_string = s.lower()
+    print(lowercase_string)
+
+    
+    for character in lowercase_string:
+        if character in punctuation_to_ignore:
+            lowercase_string = lowercase_string.replace(character, "")
+
+    print(lowercase_string)
+
+    split_string = lowercase_string.split()
+
+    # for word in split_string:
+    #     for character in word:
+    #         if character in punctuation_to_ignore:
+    #             word = word.replace(character, "")
+
+
+################### With Regex ###################
+
+    # split_string = re.findall(r'\w+', s)
+
+
+
+
+    print(split_string)
 
     
 
